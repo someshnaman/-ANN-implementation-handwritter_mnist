34396ca2dc7a9bff14122a4bcfc0ae72e688981c
someshnaman <66522077+someshnaman@users.noreply.github.com>
2022-01-29 12:58:02 +0530

    Data loader and config file setup

---
+++ src/training.py
@@ -0,0 +1,1 @@
+from src.utils.data_mgmt import get_data
---
+++ src/utils/config_reader.py
@@ -0,0 +1,11 @@
+import yaml
+
+def config_reader(path):
+    """
+
+    :type path: path of the yaml file where all your configuration details is present
+    """
+    with open(path) as config_file:
+        content = yaml.safe_load(config_file)
+
+    return content
